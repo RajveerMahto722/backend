2c214d0340a93a31c57c76452bee6aa6c90c5479
rajveer <rajveermahto722@gmail.com>
2024-07-31 17:52:06 +0530

    added

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const morgan =require("morgan");
+const app = express();
+const userdata =require("./utlity/userdata.json")
+const fileURLtoPath=require("url");
+const path= require("path");
+// properties of express has been transferd into app.
+
+const PORT = 5000;
+
+app.use(express.json()); // use take one paremeter at a time
+
+//static file wala middleware
+app.use(express.static(path.resolve(__dirname,"./Public")));
+
+app.get("/userdata", (req, res) => { /// it take 2 paremeter. 1 route 1 one function 
+  res.send(userdata);
+});
+app.get("/Public", (req, res) => { 
+  res.sendFile(path.resolve(__dirname, "./Public", "index.html"));
+});
+app.get("/contacts", (req, res) => { 
+  res.sendFile(path.resolve(__dirname, "./Public", "contacts.html"));
+});
+app.get("/stopwatch", (req, res) => { 
+  res.sendFile(path.resolve(__dirname, "./Public", "stopwatch.html"));
+});
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`.yellow.bold)
+);
