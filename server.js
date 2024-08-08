e0cec25f5663971efa562ae32a1a7f358f102b04
rajveer <rajveermahto722@gmail.com>
2024-08-08 15:49:01 +0530

    changes done

--- server.js
+++ server.js
@@ -1,32 +1,23 @@
-const express = require("express");
+const express = require("express"); 
 const colors = require("colors");
 const dotenv = require("dotenv");
-const morgan =require("morgan");
-const app = express();
-const userdata =require("./utlity/userdata.json")
-const fileURLtoPath=require("url");
-const path= require("path");
-// properties of express has been transferd into app.
+const morgan = require("morgan");
+const userdata = require("./utility/userdata.json");
+const path = require("path");
 
+const app = express();
 const PORT = 5000;
 
-app.use(express.json()); // use take one paremeter at a time
-
-//static file wala middleware
-app.use(express.static(path.resolve(__dirname,"./Public")));
-
-app.get("/userdata", (req, res) => { /// it take 2 paremeter. 1 route 1 one function 
+app.get("/userdata", (req, res) => {
   res.send(userdata);
+  });
+
+app.get("/", (req, res) => {
+  res.send( "Hello World");
 });
-app.get("/", (req, res) => { 
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
-});
-app.get("/contacts", (req, res) => { 
-  res.sendFile(path.resolve(__dirname, "./public", "contacts.html"));
-});
-app.get("/stopwatch", (req, res) => { 
-  res.sendFile(path.resolve(__dirname, "./public", "stopwatch.html"));
-});
-app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`.yellow.bold)
-);
+
+app.listen(PORT, () =>{
+    console.log(`Server running on port ${PORT}`.yellow)
+    });
+
+      
